cf260d5c2fd54082b65a1eec666d0883a1a400e2
Behram ÇELEN <behramcelen@gmail.com>
2018-03-01 04:05:42 +0100

    beautify main cpp file

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -4,111 +4,109 @@
 #include <sstream>
 
 using namespace std;
-int i,j,k,b,s;
+
+int i, j, k, b, s;
+
 void lineChars(int size, char * character) {
-    for(i = 0; i < size; i++)
-    {
-        cout << character;
-    }
-}
-void drawTriangle(int size){
-    for(i = 1; i <= size; i++){
-            lineChars(i, "X");
-      cout << "\n";
-    }
+	for (i = 0; i < size; i++) {
+		cout << character;
+	}
 }
 
-void drawSquare(int size){
-    lineChars(size, "X");
-    cout << "\n";
-    for(b = 2; b < size; b++){
-        lineChars(1, "X");
-        lineChars(size-2, " ");
-        lineChars(1, "X");
-        cout << "\n";
-
-    }
-    lineChars(size, "X");
-    cout << "\n";
+void drawTriangle(int size) {
+	for (i = 1; i <= size; i++) {
+		lineChars(i, "X");
+		cout << "\n";
+	}
 }
 
-void drawPyramidwithspaces(int size, int spacecount){
-    for(j = 1; j <= size; j++){
-        lineChars(size-j+ spacecount, " ");
-        lineChars(j*2-1, "X");
-        cout << "\n";
-    }
+void drawSquare(int size) {
+	lineChars(size, "X");
+	cout << "\n";
+	for (b = 2; b < size; b++) {
+		lineChars(1, "X");
+		lineChars(size - 2, " ");
+		lineChars(1, "X");
+		cout << "\n";
+
+	}
+	lineChars(size, "X");
+	cout << "\n";
 }
 
-void drawPyramid(int size){
-    drawPyramidwithspaces(size, 0);
+void drawPyramidwithspaces(int size, int spacecount) {
+	for (j = 1; j <= size; j++) {
+		lineChars(size - j + spacecount, " ");
+		lineChars(j * 2 - 1, "X");
+		cout << "\n";
+	}
 }
 
-void drawChristmasTree(int size){
-    for(k = 1; k <= size; k++){
-        drawPyramidwithspaces(k, size-k);
-    }
+void drawPyramid(int size) {
+	drawPyramidwithspaces(size, 0);
 }
 
-void drawCross(int size){
-    for(s = 1; s <= size; s++){
-        lineChars(size, " ");
-        lineChars(1, "X");
-        cout << "\n";
-    }
-    lineChars(size*2+1, "X");
-    cout << "\n";
-    for(s = 1; s <= size; s++){
-        lineChars(size, " ");
-        lineChars(1, "X");
-        cout << "\n";
-    }
+void drawChristmasTree(int size) {
+	for (k = 1; k <= size; k++) {
+		drawPyramidwithspaces(k, size - k);
+	}
 }
 
-void drawPa(int size){
-    lineChars(size, "X");
-    cout << "\n";
-    for(b = 2; b < size; b++){
-        lineChars(b-1, " ");
-        lineChars(1, "X");
-        lineChars(size-2, " ");
-        lineChars(1, "X");
-        cout << "\n";
-
-    }
-    lineChars(size-1, " ");
-    lineChars(size, "X");
-    cout << "\n";
+void drawCross(int size) {
+	for (s = 1; s <= size; s++) {
+		lineChars(size, " ");
+		lineChars(1, "X");
+		cout << "\n";
+	}
+	lineChars(size * 2 + 1, "X");
+	cout << "\n";
+	for (s = 1; s <= size; s++) {
+		lineChars(size, " ");
+		lineChars(1, "X");
+		cout << "\n";
+	}
 }
 
-void drawFigureX(int size){
+void drawPa(int size) {
+	lineChars(size, "X");
+	cout << "\n";
+	for (b = 2; b < size; b++) {
+		lineChars(b - 1, " ");
+		lineChars(1, "X");
+		lineChars(size - 2, " ");
+		lineChars(1, "X");
+		cout << "\n";
+
+	}
+	lineChars(size - 1, " ");
+	lineChars(size, "X");
+	cout << "\n";
 }
 
-void drawFigureY(int size){
+void drawFigureX(int size) {}
+
+void drawFigureY(int size) {}
+
+void drawFigureZ(int size) {}
+
+void drawFigureW(int size) {}
+
+bool isCommand(const string command,
+			   const char * mnemonic) {
+	return command == mnemonic;
 }
 
-void drawFigureZ(int size){
-}
-
-void drawFigureW(int size){
-}
-
-bool isCommand(const string command,const char *mnemonic){
-	return command==mnemonic;
-}
-
-int main(){
+int main() {
 	string line;
 	string command;
 	int value;
 	cout << "START" << endl;
-	while(true){
-//		cin.getline(line, MAXLINE);
-		getline(cin,line);
+	while (true) {
+		//		cin.getline(line, MAXLINE);
+		getline(cin, line);
 		std::stringstream stream(line);
 		stream >> command;
-		if(line=="" || command[0]=='#')
-		{
+		if (line == "" || command[0] == '#') {
 			// ignore empty line and comment
 			continue;
 		}
@@ -117,10 +115,10 @@ int main(){
 		cout << "!" << line << endl;;
 
 		// change to uppercase
-		command[0]=toupper(command[0]);
-		command[1]=toupper(command[1]);
+		command[0] = toupper(command[0]);
+		command[1] = toupper(command[1]);
 
-		if(isCommand(command,"HA")){
+		if (isCommand(command, "HA")) {
 			cout << "END OF EXECUTION" << endl;
 			break;
 		}
@@ -128,58 +126,47 @@ int main(){
 		// read next argument, one int value
 		stream >> value;
 
-		if(isCommand(command,"TR"))
-		{
+		if (isCommand(command, "TR")) {
 			drawTriangle(value);
 			continue;
 		}
-		if(isCommand(command,"SQ"))
-		{
+		if (isCommand(command, "SQ")) {
 			drawSquare(value);
 			continue;
 		}
-		if(isCommand(command,"PY"))
-		{
+		if (isCommand(command, "PY")) {
 			drawPyramid(value);
 			continue;
 		}
-		if(isCommand(command,"CT"))
-		{
+		if (isCommand(command, "CT")) {
 			drawChristmasTree(value);
 			continue;
 		}
-		if(isCommand(command,"FX"))
-		{
+		if (isCommand(command, "FX")) {
 			drawFigureX(value);
 			continue;
 		}
-		if(isCommand(command,"FY"))
-		{
+		if (isCommand(command, "FY")) {
 			drawFigureY(value);
 			continue;
 		}
-		if(isCommand(command,"FZ"))
-		{
+		if (isCommand(command, "FZ")) {
 			drawFigureZ(value);
 			continue;
 		}
-		if(isCommand(command,"FW"))
-		{
+		if (isCommand(command, "FW")) {
 			drawFigureW(value);
 			continue;
 		}
-		if(isCommand(command,"CR"))
-		{
+		if (isCommand(command, "CR")) {
 			drawCross(value);
 			continue;
 		}
-		if(isCommand(command,"PA"))
-		{
+		if (isCommand(command, "PA")) {
 			drawPa(value);
 			continue;
 		}
 
 		cout << "wrong argument in test: " << command << endl;
 	}
-
 }
