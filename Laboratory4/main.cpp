36299b15bbfc5f3b21dae5d7363dfdecc0116607
Behram ÇELEN <behramcelen@gmail.com>
2018-03-20 01:45:06 +0100

    revise lab4 main file

--- Laboratory4/main.cpp
+++ Laboratory4/main.cpp
@@ -1,167 +1,125 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
+#include <sstream>
 
 using namespace std;
 
 struct Element{
-	int key;
-	int value;
+  int key;
+  int value;
 };
 
 struct ElementLL{
-	Element elem;
-	ElementLL *next, *prev;
+  Element elem;
+  ElementLL *next, *prev;
 };
 
 struct List2W{
-	ElementLL *sentinel;
+  ElementLL *sentinel;
 };
 
 void init(List2W& l){
-	l.sentinel = new ElementLL;
-	ElementLL *sent = l.sentinel;
-	sent->next = sent;
-	sent->prev = sent;
+  l.sentinel = new ElementLL();
+  l.sentinel -> next = l.sentinel;
+  l.sentinel -> prev = l.sentinel;
 }
-
 void insertElem(List2W & l, Element elem){
-	ElementLL *sent = l.sentinel;
-	ElementLL *fresh = new ElementLL;
-	ElementLL *search = l.sentinel->next;
-	fresh->elem.key = elem.key;
-	fresh->elem.value = elem.value;
-
-	if (sent->next == sent)
-	{
-		fresh->next = sent;
-		fresh->prev = sent;
-		sent->next = fresh;
-		sent->prev = fresh;
-	}
-	else
-	{
-		while (search->elem.key <= fresh->elem.key && search != sent)
-
-			search=search->next;
-
-		fresh->next = search;
-		fresh->prev = search->prev;
-		fresh->next->prev = fresh;
-		fresh->prev->next = fresh;
-	}
+  ElementLL *n = new ElementLL();
+  n -> elem = elem;
+  ElementLL *i = l.sentinel;
+  while(i->prev->elem.key > n->elem.key && i->prev != l.sentinel) {
+    i = i->prev;
+  }
+  i->prev -> next = n;
+  n -> next = i;
+  n -> prev = i->prev;
+  i -> prev = n;
 }
+
+void insertElemSec(List2W & l, Element elem){
+  ElementLL *n = new ElementLL();
+  n -> elem = elem;
+  ElementLL *i = l.sentinel;
+
+  i->prev -> next = n;
+  n -> next = i;
+  n -> prev = i->prev;
+  i -> prev = n;
+}
+
 
 bool findKey(List2W & l,int key, Element &elem){
-	ElementLL *search = l.sentinel->next;
-	ElementLL *sent = l.sentinel;
-	while(search != sent)
-	{
-		if (search->elem.key == key)
-		{
-			elem = search->elem;
-			return true;
-		}
-		search=search->next;
-	}
-	return false;
+  if(l.sentinel -> next == l.sentinel)
+	  return false;
+  ElementLL *n = l.sentinel->next;
+  while(n != l.sentinel){
+    if(n->elem.key == key){
+      elem = n->elem;
+      return true;
+    }
+    n = n->next;
+  }
+  return false;
 }
 
 void removeAllKeys(List2W& l, int key){
-	ElementLL *search = l.sentinel->next;
-	ElementLL *sent = l.sentinel;
-	ElementLL *temp;
-	while(search != sent)
-	{
-		if (search->elem.key == key)
-		{
-			temp = search;
-			search=search->next;
-			temp->prev->next = temp->next;
-			temp->next->prev = temp->prev;
-			delete temp;
-			continue;
-		}
-		search=search->next;
-	}
+  if(l.sentinel -> next == l.sentinel)
+	  return;
+  ElementLL *n = l.sentinel-> next;
+
+  while(n != l.sentinel){
+
+    if(n->elem.key == key){
+      n->prev -> next = n->next;
+      n->next -> prev = n->prev;
+      ElementLL *tmp = n;
+      n = n->next;
+      delete tmp;
+    }
+    else {
+      n = n->next;
+    }
+  }
 }
 
 void showListFromHead(List2W& l){
-	ElementLL *search = l.sentinel->next;
-	ElementLL *sent = l.sentinel;
-	while(search != sent)
-	{
-		cout<<search->elem.key<<"("<<search->elem.value<<"),";
-		search=search->next;
-	}
-	cout<<endl;
+  for(ElementLL *n = l.sentinel->next; n != l.sentinel; n = n->next){
+    cout << n->elem.key << "(" << n->elem.value << "),";
+  }
+  cout << endl;
+
 }
 
 void showListFromTail(List2W& l){
-	ElementLL *search = l.sentinel->prev;
-	ElementLL *sent = l.sentinel;
-	while(search != sent)
-	{
-		cout<<search->elem.key<<"("<<search->elem.value<<"),";
-		search=search->prev;
-	}
-	cout<<endl;
+  for(ElementLL *n = l.sentinel->prev; n != l.sentinel; n = n->prev){
+    cout << n->elem.key << "(" << n->elem.value << "),";
+  }
+  cout << endl;
 }
 
 void clearList(List2W& l){
-	ElementLL *search = l.sentinel->next;
-	ElementLL *sent = l.sentinel;
-	ElementLL *temp;
-	while(search != sent)
-	{
-		temp = search;
-		search=search->next;
-		temp->prev->next = temp->next;
-		temp->next->prev = temp->prev;
-		delete temp;
-	}
+  for(ElementLL *tmp = l.sentinel->next; tmp != l.sentinel; tmp=l.sentinel->next){
+    l.sentinel->next = l.sentinel->next->next;
+    delete tmp;
+  }
+  init(l);
 }
 
 void addList(List2W& l1,List2W& l2){
-	if (l1.sentinel != l2.sentinel && l2.sentinel != l2.sentinel->next)
-	{
-		ElementLL *search1 = l1.sentinel->next;
-		ElementLL *sent1 = l1.sentinel;
-		ElementLL *search2 = l2.sentinel->next;
-		ElementLL *sent2 = l2.sentinel;
-		ElementLL *temp;
-		if (sent1 == sent1->next)
-		{
+	if(l2.sentinel->next == l2.sentinel)
+		return;
+	if(l1.sentinel == l2.sentinel)
+	   	return;
 
-			sent1->next=sent2->next;
-			sent1->prev=sent2->prev;
-			sent1->next->prev=sent1;
-			sent1->prev->next=sent1;
-			sent2->next=sent2;
-			sent2->prev=sent2;
-		}
-		else
-		{
-			while (search2 != sent2)
-			{
-				temp = search2;
-				search2=search2->next;
+	    ElementLL *n = l2.sentinel -> next;
 
-				while (temp->elem.key >= search1->elem.key && search1 != sent1)
+	    while(n != l2.sentinel)
+	    {
+	        insertElem(l1, n->elem);
+	        n = n->next;
+	    }
 
-					search1=search1->next;
-
-				temp->next = search1;
-				temp->prev = search1->prev;
-				temp->next->prev = temp;
-				temp->prev->next = temp;
-
-			}
-
-
-			sent2->next = sent2;
-			sent2->prev = sent2;
-		}
-	}
+	    clearList(l2);
 }
 
 void showBool(bool val){
@@ -171,58 +129,68 @@ void showBool(bool val){
 		cout << "false" << endl;
 }
 
+bool isCommand(const string command,const char *mnemonic){
+	return command==mnemonic;
+}
+
 int main(){
-	const int commandSize=2;
-	const int MAXLINE=100;
-	char line[MAXLINE];
-	char command[commandSize+1];
-	List2W *list;
-	list=new List2W[1];
+	string line;
+	string command;
+	List2W *list = NULL;
 	int currentL=0;
 	int value;
-	command[commandSize]='\0';
+	cout << "START" << endl;
 	while(true){
-		cin >> command[0];
-		if(command[0]=='#')
+		getline(cin,line);
+		std::stringstream stream(line);
+		stream >> command;
+		if(line=="" || command[0]=='#')
 		{
-			cin.getline(line, MAXLINE);
+			// ignore empty line and comment
 			continue;
 		}
-		cin >> command[1];
 
-		if(strcmp(command,"HA")==0){
+		// copy line on output with exclamation mark
+		cout << "!" << line << endl;;
+
+		// change to uppercase
+		command[0]=toupper(command[0]);
+		command[1]=toupper(command[1]);
+
+		// zero-argument command
+		if(isCommand(command,"HA")){
 			cout << "END OF EXECUTION" << endl;
 			break;
 		}
 
-		if(strcmp(command,"ST")==0)
-		{
-			showListFromTail(list[currentL]);
-			continue;
-		}
-
-		if(strcmp(command,"SH")==0)
+		if(isCommand(command,"SH")) //*
 		{
 			showListFromHead(list[currentL]);
 			continue;
 		}
 
-		if(strcmp(command,"CL")==0)
+		if(isCommand(command,"ST")) //*
+		{
+			showListFromTail(list[currentL]);
+			continue;
+		}
+
+		if(isCommand(command,"CL")) //*
 		{
 			clearList(list[currentL]);
 			continue;
 		}
 
-		if(strcmp(command,"IN")==0)
+		if(isCommand(command,"IN")) //*
 		{
 			init(list[currentL]);
 			continue;
 		}
 
+		// read next argument, one int value
+		stream >> value;
 
-		cin >> value;
-
-		if(strcmp(command,"FK")==0)
+		if(isCommand(command,"FK"))
 		{
 			Element elem;
 			bool ret=findKey(list[currentL], value, elem);
@@ -233,23 +201,22 @@ int main(){
 			continue;
 		}
 
-		if(strcmp(command,"RK")==0)
+		if(isCommand(command,"RK"))
 		{
 			removeAllKeys(list[currentL],value);
 			continue;
 		}
 
-
-		if(strcmp(command,"CH")==0)
+		if(isCommand(command,"CH"))
 		{
 			currentL=value;
 			continue;
 		}
 
-		if(strcmp(command,"IE")==0)
+		if(isCommand(command,"IE"))
 		{
 			int variable2;
-			cin >> variable2;
+			stream >> variable2;
 			Element elem;
 			elem.key=value;
 			elem.value=variable2;
@@ -257,18 +224,18 @@ int main(){
 			continue;
 		}
 
-		if(strcmp(command,"GO")==0)
+		if(isCommand(command,"GO"))
 		{
 			list=new List2W[value];
 			continue;
 		}
 
-		if(strcmp(command,"AD")==0)
+		if(isCommand(command,"AD"))
 		{
 			addList(list[currentL],list[value]);
 			continue;
 		}
 		cout << "wrong argument in test: " << command << endl;
-		return 1;
 	}
+	return 0;
 }
